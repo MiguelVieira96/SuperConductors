220763693bd0cf432592eaeb10b505ffc13d8081
MiguelVieira96 <83878875+MiguelVieira96@users.noreply.github.com>
2021-05-16 16:00:54 +0100

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-  <footer class="footer colored-section" id="footer">
-    <div class="container-fluid">
-      <a href="https://www.fct.unl.pt/" target="_blank">
-        <img src="images/nova.png" alt="nova-logo" class="footer-image">
-      </a>
-      <p>© Copyright 2021 SuperConductors</p>
-    </div>
-  </footer>
